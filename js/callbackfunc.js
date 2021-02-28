4b2eddc891da27f2468a8955650d1a74fc615d2a
John <exemp@gmail.com>
2021-02-28 16:59:33 +0200

    function

---
+++ js/callbackfunc.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function first() {
+    setTimeout(function () {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2)
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Im learn ${lang}`);
+    callback();
+}
+learnJS('JavaScript', function () {
+    console.log('Im go this lesson')
+})
